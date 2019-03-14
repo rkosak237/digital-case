775fd6a2ffeba36f13c1263570b0684b4b09bd3c
Robert <robert.kosak237@gmail.com>
2019-03-15 00:11:42 +0100

    Separated scss on files on smaller pieces. Now there is separated file for error messages and for media queries

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
 
 export default () => (
-    <div className="content-container content-container__copy">
+<article className="content-container content-container__copy">
     <h3 className="content-container__header">Dołącz do naszej fantastycznej aplikacji, dzieki której znajdzesz fascynujące szlaki turystyczne.</h3>
 
     <p className="content-container__text">
-    Wyszukujemy szlaki w Polsce Europie oraz Stanach Zjednoczonych, w naszej bazie mamy ponad <span className="content-container__text--bold"> 3000 szlaków</span> o łącznej
+    Wyszukujemy szlaki w Polsce Europie oraz Stanach Zjednoczonych, w naszej bazie mamy ponad <strong className="content-container__text--bold"> 3000 szlaków</strong> o łącznej
     długości
-    <span className="content-container__text--bold"> 43021 km</span>
+    <strong className="content-container__text--bold"> 43021 km</strong>
     </p>
-</div>
+</article>
 
 );
